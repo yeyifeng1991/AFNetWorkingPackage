c1a65e370201549c2ec362b7f6edd3f82dad4d21
yeyifeng <yifeng.ye@suanzi.cn>
2018-04-03 15:43:54 +0800

    增加全局单例文件

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+
+target '单列讲解' do
+pod 'AFNetworking'
+end
+
---
+++ 单列讲解/YZSingletonH.h
@@ -0,0 +1,78 @@
+//
+//  YZSingletonH.h
+//  单列讲解
+//
+//  Created by YeYiFeng on 2018/4/3.
+//  Copyright © 2018年 叶子. All rights reserved.
+//
+
+#ifndef YZSingletonH_h
+#define YZSingletonH_h
+// .h文件
+#define YZSingletonH(name) + (instancetype)shared##name;
+
+// .m文件
+#if __has_feature(objc_arc)
+
+#define YZSingletonM(name) \
+static id _instace; \
+\
++ (id)allocWithZone:(struct _NSZone *)zone \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instace = [super allocWithZone:zone]; \
+}); \
+return _instace; \
+} \
+\
++ (instancetype)shared##name \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instace = [[self alloc] init]; \
+}); \
+return _instace; \
+} \
+\
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return _instace; \
+}
+
+#else
+
+#define XHSingletonM(name) \
+static id _instace; \
+\
++ (id)allocWithZone:(struct _NSZone *)zone \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instace = [super allocWithZone:zone]; \
+}); \
+return _instace; \
+} \
+\
++ (instancetype)shared##name \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instace = [[self alloc] init]; \
+}); \
+return _instace; \
+} \
+\
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return _instace; \
+} \
+\
+- (oneway void)release { } \
+- (id)retain { return self; } \
+- (NSUInteger)retainCount { return 1;} \
+- (id)autorelease { return self;}
+
+#endif
+
+#endif /* YZSingletonH_h */
